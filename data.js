982ef27e1cb9c3ab75eb406daeaadedcd55723d2
luciafonsecadesign <luciafonsecadesign@gmail.com>
2024-10-21 03:31:32 -0500

    inicio

---
+++ data.js
@@ -0,0 +1,313 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cocina",
+      "name": "Cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.3799486251116022,
+        "pitch": 0.413471396711504,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5314190635228186,
+          "pitch": 0.16509642670971125,
+          "rotation": 11.780972450961727,
+          "target": "6-zropas"
+        },
+        {
+          "yaw": -0.30105412802315,
+          "pitch": -0.03330733782417106,
+          "rotation": 0,
+          "target": "1-estudio-camilo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-estudio-camilo",
+      "name": "Estudio Camilo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.13291353534417638,
+        "pitch": 0.27310718073829854,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.661268873738269,
+          "pitch": 0.09872631538131849,
+          "rotation": 5.497787143782138,
+          "target": "0-cocina"
+        },
+        {
+          "yaw": -3.1086332686391476,
+          "pitch": 0.3193907970779648,
+          "rotation": 0,
+          "target": "2-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sala",
+      "name": "Sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.9241444813606208,
+        "pitch": 0.4602710881248946,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1122395472258155,
+          "pitch": 0.06153336243740881,
+          "rotation": 0.7853981633974483,
+          "target": "0-cocina"
+        },
+        {
+          "yaw": -1.9743831368917917,
+          "pitch": 0.22591707013205564,
+          "rotation": 5.497787143782138,
+          "target": "1-estudio-camilo"
+        },
+        {
+          "yaw": 0.15333540078536956,
+          "pitch": 0.10448494341238046,
+          "rotation": 1.5707963267948966,
+          "target": "3-cuarto"
+        },
+        {
+          "yaw": -0.18032619235414238,
+          "pitch": 0.1301370891038669,
+          "rotation": 5.497787143782138,
+          "target": "5-bao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cuarto",
+      "name": "Cuarto",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.26186712140216883,
+        "pitch": 0.28061111523341076,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9066926728701645,
+          "pitch": 0.07325294610924615,
+          "rotation": 5.497787143782138,
+          "target": "2-sala"
+        },
+        {
+          "yaw": -1.630702341716578,
+          "pitch": 0.17790913863421487,
+          "rotation": 0,
+          "target": "4-estudio-valentina"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-estudio-valentina",
+      "name": "Estudio Valentina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.06712804815363071,
+        "pitch": 0.3259080138464743,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7658673353662273,
+          "pitch": 0.06224651119157798,
+          "rotation": 0,
+          "target": "3-cuarto"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bao",
+      "name": "Baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4476468711821644,
+          "pitch": 0.17724573976379432,
+          "rotation": 0,
+          "target": "2-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-zropas",
+      "name": "Z.Ropas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.32150384500380014,
+        "pitch": 0.40725226432910944,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.19575568037666,
+          "pitch": 0.24030446499740776,
+          "rotation": 0,
+          "target": "0-cocina"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Camilo y Valentina",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
